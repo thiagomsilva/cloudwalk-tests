9b13bb10ecc6b248d996b1708fe4d15dddf21295
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-07-09 16:16:39 -0300

    refactor: inserting comments in the code

--- app/controllers/anti_fraud_controller.rb
+++ app/controllers/anti_fraud_controller.rb
@@ -2,10 +2,13 @@ class AntiFraudController < ApplicationController
   def check_transaction
     transaction_data = params[:transaction]
 
+    # Verifica se o usuário tentou muitas transações em sequência
     if too_many_transactions_in_sequence?(transaction_data[:user_id])
       render_response(transaction_data[:transaction_id], "deny")
+    # Verifica se a transação está acima de um determinado valor em um dado período
     elsif transaction_amount_exceeds_limit?(transaction_data[:user_id], transaction_data[:transaction_amount])
       render_response(transaction_data[:transaction_id], "deny")
+    # Verifica se o usuário teve um chargeback anteriormente
     elsif user_has_chargeback?(transaction_data[:user_id])
       render_response(transaction_data[:transaction_id], "deny")
     else
@@ -19,6 +22,7 @@ class AntiFraudController < ApplicationController
     transactions = Transaction.where(user_id: user_id).order(created_at: :desc).limit(3)
     transactions.count >= 3 && transactions.first.transaction_date - transactions.last.transaction_date <= 1.hour
   end
+
 
   def transaction_amount_exceeds_limit?(user_id, amount)
     recent_transactions = Transaction.where(user_id: user_id, transaction_date: 1.day.ago..Time.now)
